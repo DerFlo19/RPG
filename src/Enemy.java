384ba8f32d5d67719afbefc9afc58d9ef5e71704
Florian <florian.hagemann07@gmail.com>
2021-03-10 17:10:55 +0100

    Player setup
    Setting up the player

--- src/Enemy.java
+++ src/Enemy.java
@@ -6,13 +6,10 @@ public class Enemy {
 	private int maxHp;  // Max hp
 	
 	public Enemy(String _name, int _hp) {
-		
 		// Assigning Attributes
 		name = _name;
 		hp = _hp;
 		maxHp = _hp;
-		
-		
 	}
 	
 	public int getHp() {
