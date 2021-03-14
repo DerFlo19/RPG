b52022413b8a49cd3dd5907ba6cd2e079ded1dab
Florian <florian.hagemann07@gmail.com>
2021-03-14 16:23:30 +0100

    Main Menu
    Everything is now ready for version  1

--- src/Fight.java
+++ src/Fight.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,14 +8,21 @@ public class Fight {
 	
 	private Enemy enemy;
 	private Player player;
+	private ArrayList<Item> loot;
+	private boolean fighting;
+	private Scanner s;
 	
 	public Fight(Enemy _enemy, Player _player) {
+		
 		enemy = _enemy;
 		player = _player;
+		loot = new ArrayList<Item>();
+		loot.add(new Item("Bones", 5));
+		loot.add(new Item("Bones", 5));
+		loot.add(new Item("Bones", 5));
+		s = new Scanner(System.in);
 		
-		fight();
 	}
-	
 	private static int getRandomNumberInRange(int min, int max) {
 
         if (min >= max) {
@@ -25,23 +33,26 @@ public class Fight {
         return r.nextInt((max - min) + 1) + min;
     }
 	
-	private void fight() {
-		
-		Scanner s = new Scanner(System.in);
-		
+	public int fight() {
+				
 		System.out.println(enemy.getName() + " is attacking you!");
 		
-		boolean fighting = true;
+		fighting = true;
 		while(fighting) {
+			
+			System.out.println("\n");
 			
 			if(player.getHp() <= 0) {
 				System.out.println("You have died!");
-				fighting = false;
-				break;
+				return 0;
 			} else if(enemy.getHp() <= 0) {
 				System.out.println("You have won!");
-				fighting = false;
-				break;
+				
+				int gold = getRandomNumberInRange(0, 25);
+				System.out.println("You got " + gold + " gold for winning!");
+				player.addGold(gold);
+				
+				return 1;
 			}
 			
 			System.out.println("You: " + player.getHp() + " HP");
@@ -51,12 +62,13 @@ public class Fight {
 			for(int i = 0; i < player.getWeaponList().size(); i++) {
 				System.out.println(i + " - " + player.getWeapon(i).getName());
 			}
-			if(s.hasNextLine()) {
+			
+			if(s.hasNext()) { 
 				
 				int choice = s.nextInt();
 				int hit = getRandomNumberInRange(0, 50);
 				hit += player.getWeapon(choice).getPrecision();
-				
+					
 				if(hit >= enemy.getAc()) {
 					
 					enemy.damage(player.getWeapon(choice).getDmg());
@@ -65,28 +77,30 @@ public class Fight {
 				} else {
 					
 					System.out.println("You missed!");
+						
+				}
+				
+				int hitE = getRandomNumberInRange(0, 50);
+				hitE += enemy.getWeapon(0).getPrecision();
+				
+				if(hitE >= enemy.getAc()) {
+					
+					player.damage(enemy.getWeapon(0).getDmg());
+					System.out.println("The enemy attacked you and caused " + player.getWeapon(0).getDmg() + " damage");
+					
+				} else {
+					
+					System.out.println("The enemy missed!");
 					
 				}
 				
-			}
-			
-			int hit = getRandomNumberInRange(0, 50);
-			hit += enemy.getWeapon(0).getPrecision();
-			
-			if(hit >= enemy.getAc()) {
-				
-				player.damage(enemy.getWeapon(0).getDmg());
-				System.out.println("The enemy attacked you and caused " + player.getWeapon(0).getDmg() + " damage");
-				
 			} else {
-				
-				System.out.println("The enemy missed!");
-				
-			}
+				System.out.println("error");
+			}		
 			
 		}
 		
-		s.close();
+		return 0;
 		
 	}
 	
--- src/main.java
+++ src/main.java
@@ -1,12 +1,113 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+	
+	private static int getRandomNumberInRange(int min, int max) {
 
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+	
 	public static void main(String[] args) {
 		
-		Player player = new Player("Florian", 100, 0, 30);
-		player.equipWeapon(new Weapon("Sword", 10, 20, 30));
-		Enemy enemy = new Enemy("EEEEE", 100, new Weapon("e", 10, 10, 10), 30);
-		Fight f = new Fight(enemy, player);
+		System.out.println("RPG - by derFlo19");
+		System.out.println("1 - Start");
+		System.out.println("2 - Leave");
+		
+		Scanner s = new Scanner(System.in);
+		int choice = s.nextInt();
+		
+		if(choice == 1) {
+			
+			System.out.println("\nWhat Text colour do you want?");
+			System.out.println("1 - White");
+			System.out.println("2 - Green");
+			
+			int colour = s.nextInt();
+			if(colour == 2) {
+			
+				try {
+					Process p = Runtime.getRuntime().exec("color a");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
+			System.out.println("\nWhat is your name?");
+			String name = "Player";
+			if(s.hasNext()) {
+				name = s.next();
+			}
+			
+			if(name.equals("jon") || name.equals("Jon")) {
+				System.out.println("Jon hat einen Kackefetisch lol");
+			}
+			
+			System.out.println("\nWhat class do you want to play, " + name + "?");
+			System.out.println("1 - Tank - high defense");
+			System.out.println("2 - Fighter - high damage");
+			System.out.println("3 - Knight - average everything");
+			
+			Player player = null;
+			int classChoice = s.nextInt();
+			if(classChoice == 1) {
+				player = new Player(name, 200, 0, 75);
+				player.equipWeapon(new Weapon("Long Sword", 0, 20, 20));
+			} else if(classChoice == 2) {
+				player = new Player(name, 100, 0, 35);
+				player.equipWeapon(new Weapon("Short Sword", 0, 10, 40));
+			} else if(classChoice == 3) {
+				player = new Player(name, 150, 0, 50);
+				player.equipWeapon(new Weapon("Sword", 0, 15, 30));
+			}
+			
+			System.out.println("Everything is now setup!\nYou will now start your first fight");
+			
+			ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+			
+			
+//			enemies.add(new Enemy("Knight", 120, new Weapon("Sword", 0, 40, 30), 40));
+			enemies.add(new Enemy("Goblin", 70, new Weapon("Mace", 0, 20, 10), 20));
+			enemies.add(new Enemy("Goblin", 70, new Weapon("Mace", 0, 20, 10), 20));
+			
+			boolean playing = true;
+			while(playing) {
+				
+				Scanner sc = new Scanner(System.in);
+				Enemy e = enemies.get(getRandomNumberInRange(0, enemies.size() - 1));
+				Fight f1 = new Fight(e, player);
+				
+				int alive = f1.fight();
+				if(alive == 0) {
+					System.out.println("Restart?");
+					System.out.println("1 - yes");
+					System.out.println("2 - no");
+					if(sc.hasNext()) {
+						int restart = sc.nextInt();
+						if(restart == 1) {
+							continue;
+						} else {
+							break;
+						}
+					}
+				}
+				
+				player.heal(1000000);
+				e.heal(1000000000);
+				System.out.println("\n");
+				
+			}
+			
+		}
 		
 	}
 
